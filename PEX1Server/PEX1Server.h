175120dfea6b5bba6a4e245cfecdba5bc4df081f
David Thacker <c21david.thacker@usafa.edu>
2019-08-17 21:28:12 -0600

    PEX1 Server Start

---
+++ PEX1Server/PEX1Server.h
@@ -0,0 +1,20 @@
+//
+// Created by David on 8/17/2019.
+//
+
+#ifndef PEX1_PEX1_H
+#define PEX1_PEX1_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <errno.h>
+
+#define PORT 4240
+#define MAXLINE 1024
+
+#endif //PEX1_PEX1_H
