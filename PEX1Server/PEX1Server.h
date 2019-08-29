b5afb22c45b82c9edce907de58c25aacdc9e043a
David Thacker <c21david.thacker@usafa.edu>
2019-08-28 22:09:08 -0600

    Server.h side commenting done

--- PEX1Server/PEX1Server.h
+++ PEX1Server/PEX1Server.h
@@ -22,7 +22,18 @@
 
 #define PORT 4240
 #define MAXLINE 1024
+/** -------------------------------------------------------------------
+ * Read the values of a file into a integer array
+ * @param FILENAME the name of the file from which the data will be read
+ * @param numBytes the total number of bytes in the file
+ * @return a integer array containing all the bytes of the file
+ */
 int* fileRead(char* FILENAME, int numbytes);
+/** -------------------------------------------------------------------
+ * Read the number of bytes in a file
+ * @param FILENAME the name of the file from which the data will be read
+ * @return the total number of bytes in the file or a -1 if the file can not be opened
+ */
 int numBytes(char* FILENAME);
 
 #endif //PEX1_PEX1_H
